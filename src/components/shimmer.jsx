a44f324494cb5e1a2c95891926ff9b62a663cc65
devangrana18 <gurjardevang72@gmail.com>
2023-07-15 20:36:37 +0530

    addshimmer

---
+++ src/components/shimmer.jsx
@@ -0,0 +1,75 @@
+
+
+const Shimmer = () => {
+  return (
+    <div>
+       <div className="simmer-button">
+        <button>------------------------</button>
+      </div>
+      <div className="shimmer-container">
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+        <div className="simmer-card">
+          <div className="simmer-img"></div>
+          <div className="simmer-name"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
